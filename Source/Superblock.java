de6f0b83efa8608ba177eed51d9f95c741a43703
midoriww <midori.williams@yahoo.com>
2016-07-23 18:47:10 -0700

    Update Superblock.java

--- Source/Superblock.java
+++ Source/Superblock.java
@@ -21,3 +21,27 @@ public class Superblock
     {
     }
 }
+
+//------ added by midori 7/23/16 -------//
+Class SuperBlock {
+    Public int totalBlocks;		// the number of disk blocks
+    Public int totalInodes;		// the number of inodes
+    Public int freeList;		// the block number of the free list’s head
+
+    Public SuperBlock( int diskSize ) {
+    // read superblock from disk
+        Byte[] superBlock = new byte[Disk.blockSize];
+        SysLib.rawread( 0, superBlck );
+        totalBlocks = SysLib.bytes2int( superBlock, 0 );
+        totalInodes = SysLib.bytes2int( superBlock, 4 );
+        freeList = SysLib.bytes2int( superBlock, 8 );
+
+        If ( totalBlocks == diskSize && totalInodes > 0 && freeList >= 2 )
+	        return;	// disk contents are valid
+        Else {
+	    // need to format disk
+	        totalBlocks = diskSize;
+	        format( defaultInodeBlocks );
+        }
+    }
+}
