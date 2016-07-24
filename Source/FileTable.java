e8f13bd90140b0e5675e4f40208e79a6c0343a16
midoriww <midori.williams@yahoo.com>
2016-07-23 19:38:33 -0700

    Update FileTable.java

--- Source/FileTable.java
+++ Source/FileTable.java
@@ -34,10 +34,32 @@ public class FileTable
      * @param filename the file name
      * @param mode the mode such as "r", "w", "w+", or "a"
      * @return a reference to the file table entry
+     * 
+     * edited 7/23/16 by Midori
      */
     public synchronized FileTableEntry falloc(String filename, String mode)
     {
-        return null; // It needs to be modified later
+        seekPt =0
+        iNode = i;
+        iNumber = inumber;
+        count = 1; 
+ 
+        //modes available
+        mode = mode.toLowerCase();
+        if (mode.compareTo("r") == 0)
+            return READONLY;
+        if (mode.compareTo("w") == 0)
+            return WRITEONLY;
+        if (mode.compareTo("w+") == 0)
+            return READWRITE;
+        if (mode.compareTo("a") == 0)
+            return APPEND;
+        return -1;
+ 
+ 
+        iNode.count++; increment
+        if (mode == APPEND) // if mode is append
+            seekPtr = iNode.length; // seekPtr points to the end of file
     }
 
     /**
@@ -48,11 +70,25 @@ public class FileTable
      * @param e the file table entry
      * @return True if this file entry found in the table,
      * false otherwise
+     * edited on 7/23/16 by Midori
      */
     public synchronized boolean ffree(FileTableEntry e)
     {
-        return false; // It needs to be modified later
-    }
+        boolean value = table.remove(e)
+ 	    if (value ==  true) {                                                               
+            e.inode.flag=0;                                                                                                          		                	  if(e.inode.count != 0)
+	        E.inode.count--;
+            SysLib.cerr(" " + e.inode.count);
+	        e.inode.toDisk(e.iNumber); //save to disk
+                                                                           
+            //write to direct
+             e = null;
+            Return true;
+        } 
+        else {
+            System.err.println("freeing nonexistent FTE");
+            Return false;
+        }
 
     /**
      * This method clear all file table entry in the table
